51c945dffefad0531e664c674d12e01d8288bb29
Aleksandr Poluianov <mailbox112000@mail.ru>
2025-03-29 21:17:38 +0300

    develop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useEffect, useState } from "react";
+function App(){
+	class User{
+		constructor(username, posts){
+			this.username=username;
+			this.posts=posts;
+		}
+	}
+	class Post{
+		constructor(text, comments){
+			this.text=text;
+			this.comments=comments;
+		}
+	}
+	class Comment{
+		constructor(email, text){
+			this.email=email;
+			this.text=text;
+		}
+	}
+	const [IDs, setIDs] = useState([]);
+	const [Messages, setMessages] = useState([]);
+	const [Comments, setComments] = useState([]);
+	const url='https://jsonplaceholder.typicode.com/'
+	useEffect(()=>{
+		fetch(url+'users').then((res)=>res.json()).then((data)=>{
+			setIDs(data);
+		});
+		fetch(url+'posts').then((res)=>res.json()).then((data)=>{
+			setMessages(data);
+		});
+		fetch(url+'comments').then((res)=>res.json()).then((data)=>{
+			setComments(data);
+		});
+	},[]);
+	let output=[];
+	for(let i=0; i<IDs.length; ++i){
+		let user=null;
+		for(let m=0, count=0; m<Messages.length; ++m){
+			if(Messages[m].userId==IDs[i].id){
+				if(count===0){
+					user=new User('Posts from '+IDs[i].username, []);
+				}
+				let post=new Post(Messages[m].body,[]);
+				user.posts.push(post);
+				count++;
+				for(let c=0; c<Comments.length; ++c){
+					//alert(Comments[c].postId);
+					if(Comments[c].postId==Messages[m].id){
+						post.comments.push(new Comment('Comment from '+Comments[c].email+':', Comments[c].body));
+					}
+				}
+			}
+		}
+		if(user!=null) output.push(user);
+	}
+	return (
+		<div className='App'>
+			
+			{output.map(e=><ul>{e.username}{e.posts.map(el=><li>{el.text}<ul>{el.comments.map(com=><li>{com.email}<br></br>{com.text}</li>)}</ul></li>)}</ul>)}
+		</div>
+	);
 }
-
 export default App;
